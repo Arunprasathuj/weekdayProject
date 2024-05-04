bb1b28c3205a2233f6e15630bca6f8ca576b3488
Arunprasathuj <arunprasath293@gmail.com>
2024-05-04 19:23:07 +0530

    customizing with css

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,7 +1,7 @@
 // components/JobCard.js
 
 import React, { useState } from 'react';
-
+import './JobCard.css';
 function JobCard({ job }) {
   const [expanded, setExpanded] = useState(false);
 
@@ -11,17 +11,22 @@ function JobCard({ job }) {
 
   return (
     <div className="job-card">
-      <h2>{job.title}</h2>
-      <p>{job.company}</p>
-      <p>{job.location}</p>
-      <p>{expanded ? job.jobDetailsFromCompany : job.jobDetailsFromCompany.substring(0, 100)}</p>
+    <p className="posted">{job.date}</p>
+      <h2 className="title">{job.title}</h2>
+      <p className="role">{job.role}</p>
+      <p className="salary">{job.salary}</p>
+      <p className="company">{job.company}</p>
+      <p className="location">{job.location}</p>
+      <p className="content">{expanded ? job.jobDetailsFromCompany : job.jobDetailsFromCompany.substring(0, 100)}</p>
       {job.jobDetailsFromCompany.length > 100 && (
         <button onClick={toggleExpanded}>
           {expanded ? 'Show Less' : 'Show More'}
         </button>
       )}
-      <p>{job.experience}</p>
-      <button>Apply</button>
+      <p className="experience">{job.experience}</p>
+      <button className="button1">Apply</button>
+      <button className="button2">Apply</button>
+
     </div>
   );
 }
